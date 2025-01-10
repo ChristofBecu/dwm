737574699a42dbcffdec59996dd7e2407b7519e9
Christof Becu <christof.becu@gmail.com>
2025-01-10 12:39:47 +0100

    Add autostart functionality for specific applications on designated tags

--- config.def.h
+++ config.def.h
@@ -59,6 +59,13 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *browsercmd[]  = { "zen-browser", NULL };
+
+Autostarttag autostarttaglist[] = {
+	{.cmd = termcmd, .tags = 1 << 0 },
+	{.cmd = browsercmd, .tags = 1 << 0 },
+	{.cmd = NULL, .tags = 0 },
+};
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
