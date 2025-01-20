d24b9cfeec75b446d774bdf29e3d8131749cf43b
Christof Becu <christof.becu@gmail.com>
2025-01-20 22:10:14 +0100

    Add autostart script for setting desktop background

--- config.def.h
+++ config.def.h
@@ -31,6 +31,11 @@ static const Rule rules[] = {
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
+static const AutoStartScript autoStartScripts[] = {
+	/* cmd                                                          args */
+	{ "/home/bedawang/.config/dwm/scripts/fehbackground.sh",        NULL },
+};
+
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
---
+++ scripts/fehbackground.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+feh --bg-fill ~/.config/backgrounds/Black-Elegant-HD-Backgrounds.jpg
